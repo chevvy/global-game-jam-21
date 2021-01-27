2d0e55f5c8404083cc146f912f945a74232b67cc
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2021-01-27 10:07:34 -0500

    Add - Player looking in movement direction + basic cinemachineCamera

--- GameJam/Assets/Scripts/PlayerController/PlayerController.cs
+++ GameJam/Assets/Scripts/PlayerController/PlayerController.cs
@@ -7,15 +7,36 @@ namespace PlayerController {
 		
 		private Rigidbody playerRigidBody;
 		private Vector2 playerMovement;
+
+		#region Unity public fonctions
 		void Start() {
 			playerRigidBody = GetComponent<Rigidbody>();
 		}
-		
-		void FixedUpdate ()
-		{
+
+		void Update() {
+			SetDirection(playerMovement);
+		}
+
+		void FixedUpdate () {
 			Vector3 movement = new Vector3 (playerMovement.x, 0.0f, playerMovement.y);
  
 			playerRigidBody.AddForce (movement * PlayerSpeed);
+		}
+		#endregion
+		
+		/// <summary>
+		/// Sets a player direction according to where the left stick is pointing
+		/// </summary>
+		/// <param name="movement">A vector2 of current stick movement</param>
+		void SetDirection(Vector2 movement)
+		{
+			// Inverts direction for the eyes to be in the same orientation as the moving direction
+			Vector3 direction = new Vector3(-movement.x, 0f, -movement.y).normalized;
+			if (direction.magnitude >= 0.1f)
+			{
+				float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+				transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+			}
 		}
 
 		#region InputCallBack
