5ca0542963f22b2a5c7e79ca0598f610380f9e31
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2021-01-28 11:35:03 -0500

    Add - link player controller and animator
    still missing some actions

--- GameJam/Assets/Scripts/PlayerController/PlayerController.cs
+++ GameJam/Assets/Scripts/PlayerController/PlayerController.cs
@@ -7,16 +7,24 @@ namespace PlayerController {
 	public class PlayerController : MonoBehaviour {
 		[FormerlySerializedAs("PlayerSpeed")] public float playerSpeed = 2f;
 		public float turnSmoothTime = 0.1f;
+		
+		
 		private float internalSmoothingTime = 0.5f; // using this because the value from the input are too low
-		private Rigidbody playerRigidBody;
+		public Rigidbody playerRigidBody;
 		private Vector2 playerMovement;
 
 		private float turnSmoothVelocity;
-		
+
+		#region Animator
+		public Animator animator;
+		private static readonly int Move = Animator.StringToHash("move");
+		private static readonly int Dig = Animator.StringToHash("dig");
+		#endregion
 
 		#region Unity public fonctions
 		void Start() {
-			playerRigidBody = GetComponent<Rigidbody>();
+			// playerRigidBody = GetComponent<Rigidbody>();
+			// animator = GetComponent<Animator>();
 		}
 
 		void Update() {
@@ -38,7 +46,7 @@ namespace PlayerController {
 		void SetDirection(Vector2 movement)
 		{
 			// Inverts direction for the eyes to be in the same orientation as the moving direction
-			Vector3 direction = new Vector3(-movement.x, 0f, -movement.y).normalized;
+			Vector3 direction = new Vector3(movement.x, 0f, movement.y).normalized;
 			if (direction.magnitude >= 0.1f)
 			{
 				float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
@@ -55,9 +63,22 @@ namespace PlayerController {
 			
 			if(context.performed) {
 				playerMovement = context.ReadValue<Vector2>();
+				float playerMovementMagnitude = playerMovement.magnitude;
+				try
+				{
+					animator.SetFloat(Move, playerMovementMagnitude);
+				}
+				catch (Exception e)
+				{
+					Debug.Log("Nom object = " + name);
+					throw;
+				}
+				
+
 			}
 			if(context.canceled) {
 				playerMovement = Vector3.zero;
+				animator.SetFloat(Move, 0f);
 			}
 		}
 
