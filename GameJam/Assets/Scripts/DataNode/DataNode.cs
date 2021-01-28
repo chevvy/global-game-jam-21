3ed15d932fa251f1d817150c420aa60dc2cf4929
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2021-01-28 16:34:44 -0500

    Add - Spawn from above !

--- GameJam/Assets/Scripts/DataNode/DataNode.cs
+++ GameJam/Assets/Scripts/DataNode/DataNode.cs
@@ -6,29 +6,40 @@ namespace DataNode {
     public class DataNode : MonoBehaviour {
         public bool IsDataNode { set; get; }
         public int NodeID { get; set; }
+        public float dataNodeMass = 5f;
         public Material dataNodeMaterial;
-        
-        void Awake() {
+        public GameObject nodePrefab;
 
+        public GridManager gridManager;
+
+        public void SpawnDataNode() {
+            var newNode = InstantiateNewNode();
+            SetNodeRigidBodyProperties(newNode);
+            SetNodeVisuals(newNode);
         }
 
-        public void ActivateDataNode() {
-            // SetNodeRigidBodyProperties();
-            SetNodeVisuals();
+        private GameObject InstantiateNewNode() {
+            var position = transform.position;
+            Vector3 spawnPosition = new Vector3(position.x,
+                position.y + gridManager.dataNodeHeightAtSpawn, position.z);
+            var newNode = Instantiate(nodePrefab, position, Quaternion.identity);
+            newNode.transform.position = spawnPosition;
+            return newNode;
         }
 
-        private void SetNodeVisuals() {
-            GetComponent<MeshRenderer>().material = dataNodeMaterial;
-        }
-
-        private void SetNodeRigidBodyProperties() {
-            gameObject.AddComponent<Rigidbody>();
-            Rigidbody nodeRigidbody = GetComponent<Rigidbody>();
+        private void SetNodeRigidBodyProperties(GameObject node) {
+            node.AddComponent<Rigidbody>();
+            Rigidbody nodeRigidbody = node.GetComponent<Rigidbody>();
             RigidbodyConstraints constraints =
                 RigidbodyConstraints.FreezeRotation |
                 RigidbodyConstraints.FreezePositionX |
                 RigidbodyConstraints.FreezePositionZ;
             nodeRigidbody.constraints = constraints;
+            nodeRigidbody.mass = dataNodeMass;
+        }
+        
+        private void SetNodeVisuals(GameObject node) {
+            node.GetComponent<MeshRenderer>().material = dataNodeMaterial;
         }
     }
 }
--- GameJam/Assets/Scripts/Managers/GridManager.cs
+++ GameJam/Assets/Scripts/Managers/GridManager.cs
@@ -10,25 +10,27 @@ namespace Managers
     {
         public Material dataNodeMaterial;
         public Material defaultMaterial;
+        public GameObject tilePrefab;
         
         public GameObject floor;
-        private List<GameObject> tiles;
+        private List<GameObject> _tiles;
 
         public int numberOfDataNode = 20;
-        private HashSet<int> indexesOfDataNode;
-        public int dataNodeHeightAtSpawn = 100;
+        private HashSet<int> _indexesOfDataNode;
+        public int dataNodeHeightAtSpawn = 10;
+        public float dataNodeMass = 30f;
         void Awake() {
             CheckForMissingComponents();
             InitializeFloorStructure();
             GenerateRandomDataNodes(numberOfDataNode);
-            SpawnDataNode(indexesOfDataNode);
+            SpawnDataNode(_indexesOfDataNode);
         }
 
         private void GenerateRandomDataNodes(int numberOfNodes) {
             Random randomDataNodeBlocks = new Random();
             for (int i = 0; i < numberOfNodes; i++) {
-                int tileNumber = randomDataNodeBlocks.Next(0, tiles.Count);
-                indexesOfDataNode.Add(tileNumber);
+                int tileNumber = randomDataNodeBlocks.Next(0, _tiles.Count);
+                _indexesOfDataNode.Add(tileNumber);
             }
         }
 
@@ -44,18 +46,22 @@ namespace Managers
             if (floor == null) {
                 Debug.LogError("Missing floor assignment on Gridmanager");
             }
+
+            if (tilePrefab == null) {
+                Debug.LogError("Missing tile prefab");
+            }
         }
 
 
         public void SpawnDataNode(HashSet<int> indexes) {
             foreach (int index in indexes) {
-                DataNode.DataNode currentNode = tiles[index].GetComponent<DataNode.DataNode>();
-                currentNode.ActivateDataNode();
+                DataNode.DataNode currentNode = _tiles[index].GetComponent<DataNode.DataNode>();
+                currentNode.SpawnDataNode();
             }
         }
         private void InitializeFloorStructure() {
-            tiles = new List<GameObject>();
-            indexesOfDataNode = new HashSet<int>();
+            _tiles = new List<GameObject>();
+            _indexesOfDataNode = new HashSet<int>();
             int tileIndex = 0;
             foreach (Transform child in floor.transform) {
                 GameObject tile = child.gameObject;
@@ -65,7 +71,11 @@ namespace Managers
                 node.IsDataNode = false;
                 node.NodeID = tileIndex;
                 node.dataNodeMaterial = dataNodeMaterial;
-                tiles.Add(tile);
+                node.nodePrefab = tilePrefab;
+                node.gridManager = this;
+                node.dataNodeMass = dataNodeMass;
+                
+                _tiles.Add(tile);
 
                 tileIndex++;
             }
