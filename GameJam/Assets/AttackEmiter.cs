1b9465fe6433bfd08daf3efbc74a50bc80411b3e
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2021-01-30 17:56:22 -0500

    Add - floor  bg

--- GameJam/Assets/AttackEmiter.cs
+++ GameJam/Assets/AttackEmiter.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackEmiter : MonoBehaviour
-{
+public class AttackEmiter : MonoBehaviour {
+    public bool isAttacking;
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
             other.GetComponent<PlayerController.PlayerController>().GetsAttacked(transform.position);
         }
     }
+
+    // public bool SetAttackingState => isAttacking != isAttacking;
+    // faire que au debug de l'anim, on permettre l'attaque et à fin anim, on empèche lattaque
+    // si pas dans ce frame, l'attaque n'est pas passé
 }
